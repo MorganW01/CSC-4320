dade3bd7f9600e3f9b7d64c5e508f262ab7afddf
Morgan <morganw2001@gmail.com>
2022-02-23 22:18:47 -0500

    new values

--- Homework3/src/Homework3.java
+++ Homework3/src/Homework3.java
@@ -44,7 +44,7 @@ public class Homework3 {
 
                 else if(y==1){
                     //generates CPU Cycle data using random generator
-                    CPUCycles = (int) ((random.nextGaussian() *3000) + 6000);
+                    CPUCycles = (int) ((random.nextGaussian() * 3000) + 6000);
                     CPUCycles = (CPUCycles % (11000-1000) +1000);
                     processRepresentation[x][y]=CPUCycles;
                     CPUInt = processRepresentation[x][y];
@@ -53,8 +53,8 @@ public class Homework3 {
 
                 else {
                     //generates memory footprint data using random generator
-                    sizeOfMemoryFootprint = (int) ((random.nextGaussian() *50) + 20);
-                    sizeOfMemoryFootprint = (sizeOfMemoryFootprint % (100-1) +1);
+                    sizeOfMemoryFootprint = (int) ((random.nextGaussian() * 50) + 20);
+                    sizeOfMemoryFootprint = (sizeOfMemoryFootprint % (100-1) + 1);
                     processRepresentation[x][y]= sizeOfMemoryFootprint;
                     memFootprintInt=processRepresentation[x][y];
 
