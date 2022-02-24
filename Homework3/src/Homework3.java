ae657bd353b5e0df936f089c52b89f9e82d5cdd9
Morgan <morganw2001@gmail.com>
2022-02-23 21:47:50 -0500

    done with program

--- Homework3/src/Homework3.java
+++ Homework3/src/Homework3.java
@@ -1,44 +1,72 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Random;
 public class Homework3 {
 
-    public static void main(String[] args){
-        Random random = new Random ();
-
-        int k = 50; // number of processes; can be changed
-        int processID;
+    public static void main(String[] args) throws FileNotFoundException {
+        PrintWriter out = new PrintWriter("problem2.txt");
+        Random random = new Random();
+        int k = 50; // number of processes & process id. It is just the value of k from 1-50
+        int columns = 3;
         int CPUCycles;
         int sizeOfMemoryFootprint;
-
-        int lowerBound = 1000;
-        int upperBound = 11000;
-
-        //k rows, and 3 columns one for PID, CPU cycles, size of memory footprint
-        int [][] processRepresentation = new int [k][3];
+        int CPUInt=0;
+        int memFootprintInt=0;
+        int processInt=0;
 
 
-        //print statement to create table
-        System.out.println("Number of Processes = " + k);
-        System.out.println("Process ID:           CPU Cycles:             Size of Memory Footprint: ");
+        //creates 2d array with k rows and 3 columns one for PID, CPU cycles, size of memory footprint
+        int [][] processRepresentation = new int [k][columns];
 
-        //prints random number between 1000 and 11000
+        //print statement to create table header
+        out.println("Number of Processes = " + k);
+        out.printf("%-30s%-30s%-30s%n", "Process ID: ", "CPU Cycles: ","Size of Memory Footprint: ");
 
 
-        for (int i =0; i <= k; i++){ // iterates through the rows
+        //prints random number between 1000 and 1100.
+        for (int x = 0; x < k; x++){ // iterates through the rows
+            for (int y = 0; y < columns; y++){
+                //checks if we are in the first column of the array
+                if(y==0){
+                    processRepresentation [x][y] = x+1; //prints out process id/number to the first column
+                    processInt = processRepresentation[x][y];
 
-                CPUCycles = random.nextInt((11000-1000)+1) + 1000;
+                }
 
-                sizeOfMemoryFootprint = random.nextInt((100-1)+1) + 1;
+                else if(y==1){
+                    CPUCycles = random.nextInt((11000-1000)+1) + 1000;
+                    processRepresentation[x][y]=CPUCycles;
+                    CPUInt = processRepresentation[x][y];
 
+                }
+
+                else {
+                    sizeOfMemoryFootprint = random.nextInt((100-1)+1) + 1;
+                    processRepresentation[x][y]= sizeOfMemoryFootprint;
+                    memFootprintInt=processRepresentation[x][y];
+
+                }
+
+
+            }
+            out.printf("%-30s%-30s%-30s%n", processInt, CPUInt, memFootprintInt);
 
         }
 
 
+        /*to do:
+        - fix random generator
+        print it in table format with for loops
+        print into a text file
+        use excel to create graphs
 
 
+         */
 
-
-
+        out.close();
 
     }
+
+
 }
