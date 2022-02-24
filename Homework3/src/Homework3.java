9e237543d58a0fd2ee810e93229bfa3cd646d63e
Morgan <morganw2001@gmail.com>
2022-02-23 21:52:57 -0500

    added comments

--- Homework3/src/Homework3.java
+++ Homework3/src/Homework3.java
@@ -1,16 +1,21 @@
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.Random;
 public class Homework3 {
 
     public static void main(String[] args) throws FileNotFoundException {
+        //Object used to print data in the text file
         PrintWriter out = new PrintWriter("problem2.txt");
+        //Random object used to generated random numbers
         Random random = new Random();
-        int k = 50; // number of processes & process id. It is just the value of k from 1-50
+        // number of processes/process ids
+        int k = 50;
+        //number of columns in the array
         int columns = 3;
+        // ints used to store randomly generated values
         int CPUCycles;
         int sizeOfMemoryFootprint;
+        //variables used to print the table
         int CPUInt=0;
         int memFootprintInt=0;
         int processInt=0;
@@ -19,8 +24,10 @@ public class Homework3 {
         //creates 2d array with k rows and 3 columns one for PID, CPU cycles, size of memory footprint
         int [][] processRepresentation = new int [k][columns];
 
-        //print statement to create table header
+        //prints number of processes
         out.println("Number of Processes = " + k);
+
+        //prints table header
         out.printf("%-30s%-30s%-30s%n", "Process ID: ", "CPU Cycles: ","Size of Memory Footprint: ");
 
 
@@ -29,12 +36,14 @@ public class Homework3 {
             for (int y = 0; y < columns; y++){
                 //checks if we are in the first column of the array
                 if(y==0){
+                    //obtains all of the process ids which is just the the values of k (1-50)
                     processRepresentation [x][y] = x+1; //prints out process id/number to the first column
                     processInt = processRepresentation[x][y];
 
                 }
 
                 else if(y==1){
+                    //generates CPU Cycle data using random generator
                     CPUCycles = random.nextInt((11000-1000)+1) + 1000;
                     processRepresentation[x][y]=CPUCycles;
                     CPUInt = processRepresentation[x][y];
@@ -42,6 +51,7 @@ public class Homework3 {
                 }
 
                 else {
+                    //generates memory footprint data using random generator
                     sizeOfMemoryFootprint = random.nextInt((100-1)+1) + 1;
                     processRepresentation[x][y]= sizeOfMemoryFootprint;
                     memFootprintInt=processRepresentation[x][y];
@@ -50,20 +60,11 @@ public class Homework3 {
 
 
             }
+            //Uses PrintWriter object to print out the table with the data in the text file in the correct format
             out.printf("%-30s%-30s%-30s%n", processInt, CPUInt, memFootprintInt);
 
         }
-
-
-        /*to do:
-        - fix random generator
-        print it in table format with for loops
-        print into a text file
-        use excel to create graphs
-
-
-         */
-
+        //closes PrintWriter object
         out.close();
 
     }
